5b8337aac7602c3d3e95e2283c256eabadc05dbf
TahirReyaz <tahir200213@st.jmi.ac.in>
2022-02-25 09:22:38 +0530

    Bug fix

--- functions/reminders.js
+++ functions/reminders.js
@@ -2,12 +2,14 @@ const { sendText } = require("./venomFunctions");
 
 module.exports.remind = (client, time, chatId) => {
   const now = new Date();
+  console.log("time", time);
   let remTime = new Date(`${now.toLocaleDateString()} ${time}:00`) - now;
+  console.log("rembef", remTime);
   if (remTime < 0) {
     remTime += 86400000;
   }
   const msg = "Will remind you at " + time;
-  console.log(remTime);
+  console.log("remafter", remTime);
 
   const timer = setTimeout(
     () => replyAndRemove(client, timer, chatId, msg),
